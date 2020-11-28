b8eb36f56d95b7f5cc325a3b1a2644847a7aabf1
Siddharth Gupta <g.sidd97@gmail.com>
2020-11-27 17:25:21 -0800

    nuke

--- createuser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Creating database user."
-sudo -u postgres psql -c "CREATE USER cbadmin WITH PASSWORD 'cbadmin';"
-
-echo "Granting super user permissions"
-sudo -u postgres psql -c "ALTER USER cbadmin WITH SUPERUSER;"
